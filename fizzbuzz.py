5174f4d43e42bf48a310c32260e4741db0c03cc4
Ein_Green <mbananola2002@gmail.com>
2021-05-26 22:45:10 -0600

    created the fizz_buzz funtion and started by adding the FizzBuzz result first

---
+++ fizzbuzz.py
@@ -0,0 +1,6 @@
+def fizz_buzz(num):
+    result = ""
+    # I have no idea how to return a string, so Ima just make the if statements edit an empty one
+    if(num % 3 == 0 and num % 5 == 0):
+        result = result + "FizzBuzz"
+        return result
