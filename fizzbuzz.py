f4b5c27cff3ee2c992868d42418911b9e5382de5
Ein_Green <mbananola2002@gmail.com>
2021-05-26 22:47:30 -0600

    apparently the function doesn't work unless the FizzBuzz statement goes first, just figured that out, wasted time removing a git, and crying most of the day

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,12 @@
 def fizz_buzz(num):
     result = ""
     # I have no idea how to return a string, so Ima just make the if statements edit an empty one
+    if(num % 3 == 0 and num % 5 == 0):
+        result = result + "FizzBuzz"
+        return result
     if(num % 3 == 0):
         result = result + "Fizz"
         return result
     if(num % 5 == 0):
         result = result + "Buzz"
-        return result
-    if(num % 3 == 0 and num % 5 == 0):
-        result = result + "FizzBuzz"
         return result
