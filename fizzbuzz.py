71b34b8078170899fdf71d30836a64cae7074fdc
Ein_Green <mbananola2002@gmail.com>
2021-05-26 22:48:49 -0600

    added an if statement that returns the num as a string if num doesn't meet the requirements for fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -4,6 +4,11 @@ def fizz_buzz(num):
     if(num % 3 == 0 and num % 5 == 0):
         result = result + "FizzBuzz"
         return result
+    # apparently the FizzBuzz statement has to go first, otherwise the 3 or 5 will always be calculated to be true first, and run first
+    if(num % 3 != 0 and num % 5 != 0):
+        result = result + str(num)
+        return result
+    # I returned the num by itself if it doesn't meet the fizz_buzz condition. Also I made it go before the single conditions just to be safe
     if(num % 3 == 0):
         result = result + "Fizz"
         return result
